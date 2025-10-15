2c2b0318f0ce03eacfe5a35e2673d114ea9ff5ea
João Pedro Fiorindo <joaopedrofiorindopinto@gmail.com>
2025-10-15 16:16:19 -0300

    Adicionando docker ao ambiente
    Até o momento adicionamos o docker no projeto e foi ensinado como se utiliza o mesmo

--- dist/User.d.ts
+++
@@ -1,7 +0,0 @@
-export default class User {
-    readonly email: string;
-    readonly password: string;
-    constructor(email: string, password: string);
-    getEmail(): string;
-}
-//# sourceMappingURL=User.d.ts.map
