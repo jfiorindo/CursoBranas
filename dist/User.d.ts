a2273500b4eb07acb26acb71d610b179ed36d09a
João Pedro Fiorindo <joaopedrofiorindopinto@gmail.com>
2025-10-14 21:22:00 -0300

    Arrumando ambiente

---
+++ dist/User.d.ts
@@ -0,0 +1,7 @@
+export default class User {
+    readonly email: string;
+    readonly password: string;
+    constructor(email: string, password: string);
+    getEmail(): string;
+}
+//# sourceMappingURL=User.d.ts.map
