af1a53dd99fe4c909d9bffe0de70d61c32f7fdd2
João Pedro Fiorindo <joaopedrofiorindopinto@gmail.com>
2025-10-16 16:23:11 -0300

    Iniciando os testes
    Até o momento na aula de hoje, começamos a falar sobre testes, mais exatamente sobre o TDD. Temos um teste básico que conseguimos testar com o container e sem para fixar melhor a fase de container e iniciar a nova de testes. Também criei um arquivo txt que contém alguns conceitos básicos do TDD.

---
+++ test/validateCpf.test.ts
@@ -0,0 +1,24 @@
+import { validateCpf } from "../src/validateCpf";
+
+test.each([
+    //given (São os cpfs a serem testados, estamos inserindo os valores a serem passado para a string cpf)
+    "71428793860",
+    "87748248800",
+    "97456321558"
+])("Deve testar um cpf válido %s", (cpf:string) => {
+    //Act (aqui estamos executando o ato, que seria validar o cpf, que joga a variável cpf para o arquivo validaeCpf.ts e nele tem todos os parâmetros necessários para verificar)
+    const isValid = validateCpf(cpf);
+    //then (depois a gente busca a variável isValid, que ficará armazenado o resultado booleano, e caso seja true, o teste irá dar acerto, caso dê fail, o teste retornará pass)
+    expect(isValid).toBe(true);
+});
+
+
+test.each([
+    "11111111111",
+    "1111",
+    "1111111111111"
+])("Deve testar um cpf inválido %s", (cpf:string) => { 
+    // nesse teste vamos buscar testar os cpfs inválidos, que devem voltar false para o teste ser validado. Segue a mesma lógica do primeiro teste, só que para cpfs inválidos. Assim a gente aumenta o nível de aproveitamento do teste também, testando as funcionalidades de verdadeiro quanto falso
+    const isValid = validateCpf(cpf);
+    expect(isValid).toBe(false);
+});
