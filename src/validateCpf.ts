b41d7ece08ac7469bf0a2a0fcd58c64714cb298a
João Pedro Fiorindo <joaopedrofiorindopinto@gmail.com>
2025-10-20 19:37:21 -0300

    Refactoring
    Na aula de hoje aprendemos alguns conceitos de refactoring e os aplicamos com base nos testes também. Tinhamos um código de verificação de CPF totalmente mal feito, pegamos o código refizemos basicamente do 0, criamos funções, renomeamos variáveis e deixamos identado, sem contar que o teste ainda tem 100% de cobertura, deixando o código confiável e bem testado.

--- src/validateCpf.ts
+++ src/validateCpf.ts
@@ -1,60 +1,33 @@
-export function validateCpf (str: string) {
-	if (str !== null) {
-		if (str !== undefined) {
-			if (str.length >= 11 && str.length <= 14){
-				// cleaning cpf
-				str=str
-					.replace('.','')
-					.replace('.','')
-					.replace('-','')
-					.replace(" ","");  
-				// tudo igual
-				if (!str.split("").every(c => c === str[0])) {
-					try{  
-						let     d1, d2;  
-						let     dg1, dg2, rest;  
-						let     digito;  
-							let     nDigResult;  
-						d1 = d2 = 0;  
-						dg1 = dg2 = rest = 0;  
-							
-						for (let nCount = 1; nCount < str.length -1; nCount++) {  
-							// if (isNaN(parseInt(str.substring(nCount -1, nCount)))) {
-							// 	return false;
-							// } else {
-	
-								digito = parseInt(str.substring(nCount -1, nCount));  							
-								d1 = d1 + ( 11 - nCount ) * digito;  
-				
-								d2 = d2 + ( 12 - nCount ) * digito;  
-							// }
-						};  
-							
-						rest = (d1 % 11);  
-						
-						// se for menor que 2 é 0, senão é 11 menos o resto
-						dg1 = (rest < 2) ? dg1 = 0 : 11 - rest;  
-						d2 += 2 * dg1;  
-						rest = (d2 % 11);  
-						if (rest < 2)  
-							dg2 = 0;  
-						else  
-							dg2 = 11 - rest;  
-				
-							let nDigVerific = str.substring(str.length-2, str.length);  
-						nDigResult = "" + dg1 + "" + dg2;  
+const VALID_LENGTH = 11;
 
-						return nDigVerific == nDigResult;
-						
-					// se der problema...
-					}catch (e){  
-						console.error("Erro !"+e);  
-	
-						return false;  
-					}  
-				} else return false;
-	
-			}else return false;
-		} else return false;
-	} else return false;
+export function validateCpf (cpf: string) {
+	if (!cpf) return false;
+	cpf = clean(cpf);
+	if (cpf.length !== VALID_LENGTH) return false;
+	if (allDigitsTheSame(cpf)) return false;
+	const dg1 = calculateDigit(cpf, 10);
+	const dg2 = calculateDigit(cpf, 11);
+	return extractCheckDigits(cpf) == `${dg1}${dg2}`;
+}
+
+function clean (cpf: string){
+	return cpf.replace(/\D/g, "");
+}
+
+function allDigitsTheSame(cpf: string){
+	const [firstDigit] = cpf;
+	return [...cpf].every(digit => digit === firstDigit);
+}
+
+function calculateDigit (cpf: string, factor: number) {
+	let total = 0;
+	for(const digit of cpf){
+		if (factor > 1) total += parseInt(digit) * factor--;
+	}
+	const rest = total%11;
+	return (rest < 2 ) ? 0 : 11 - rest;
+}
+
+function extractCheckDigits (cpf: string){
+	return cpf.slice(9);
 }
--- test/validateCpf.test.ts
+++ test/validateCpf.test.ts
@@ -5,7 +5,7 @@ test.each([
     "71428793860",
     "87748248800",
     "97456321558"
-])("Deve testar um cpf válido %s", (cpf:string) => {
+])("Deve testar um cpf válido %s", (cpf: string) => {
     //Act (aqui estamos executando o ato, que seria validar o cpf, que joga a variável cpf para o arquivo validaeCpf.ts e nele tem todos os parâmetros necessários para verificar)
     const isValid = validateCpf(cpf);
     //then (depois a gente busca a variável isValid, que ficará armazenado o resultado booleano, e caso seja true, o teste irá dar acerto, caso dê fail, o teste retornará pass)
@@ -14,10 +14,12 @@ test.each([
 
 
 test.each([
+    null,
+    undefined,
     "11111111111",
-    "1111",
-    "1111111111111"
-])("Deve testar um cpf inválido %s", (cpf:string) => { 
+    "111",
+    "1111111111111111",
+])("Deve testar um cpf inválido %s", (cpf: any) => { 
     // nesse teste vamos buscar testar os cpfs inválidos, que devem voltar false para o teste ser validado. Segue a mesma lógica do primeiro teste, só que para cpfs inválidos. Assim a gente aumenta o nível de aproveitamento do teste também, testando as funcionalidades de verdadeiro quanto falso
     const isValid = validateCpf(cpf);
     expect(isValid).toBe(false);
